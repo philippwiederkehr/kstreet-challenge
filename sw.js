04f027d0b16fedc7b292f6e984cbeecb8361f91b
Philipp <philipp@Mac.localdomain>
2026-09-18 16:10:57 +0200

    Polish final arcade presentation

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 // KSTREET CHALLENGE - Service Worker
-const CACHE_VERSION = 'v13';
+const CACHE_VERSION = 'v14';
 const CACHE_NAME = `kstreet-${CACHE_VERSION}`;
 
 // App shell files to pre-cache on install
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=11',
+  '/style.css?v=12',
   '/script.js?v=11',
   '/favicon.svg',
   '/icons/icon-192.png',
